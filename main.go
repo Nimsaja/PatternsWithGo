d195a130108611d7e8832d9a3e74137fc8ecbcde
Nimsaja <jasmin.roeper@nexgo.de>
2019-08-06 17:15:33 +0200

    a second decorator

--- main.go
+++ main.go
@@ -7,10 +7,19 @@ import (
 	"github.com/Nimsaja/PatternsWithGo/tools"
 )
 
+var lastResult float64
+
 func main() {
 	stopTime(tools.Pi, 10)
 	stopTime(tools.Pi, 10000)
 	stopTime(tools.Pi, 1000000)
+
+	saveResult(tools.Pi, 10)
+	fmt.Println("last Result ", lastResult)
+	saveResult(tools.Pi, 10000)
+	fmt.Println("last Result ", lastResult)
+	saveResult(tools.Pi, 1000000)
+	fmt.Println("last Result ", lastResult)
 }
 
 func stopTime(a func(int) float64, n int) {
@@ -18,4 +27,9 @@ func stopTime(a func(int) float64, n int) {
 	r := a(n)
 	fmt.Printf("calculate Pi inside stopTime %v -> %v\n", n, r)
 	fmt.Println((time.Now()).Sub(s))
+}
+
+func saveResult(a func(int) float64, n int) {
+	r := a(n)
+	lastResult = r
 }

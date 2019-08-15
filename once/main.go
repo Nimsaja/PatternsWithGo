34ae9874fc7c9fbcf4653ec9c503b58c75f6c4e2
Nimsaja <jasmin.roeper@nexgo.de>
2019-08-15 14:58:17 +0200

    pass arguments to once.Do

--- once/main.go
+++ once/main.go
@@ -9,26 +9,32 @@ import (
 var doOnce sync.Once
 
 func main() {
+	pts := 7
+
 	for i := 1; i < 20; i++ {
-		fmt.Printf("Calculation with %v is %v\n", i, calc(i))
+		fmt.Printf("Calculation with %v is %v\n", i, calc(i, pts))
 	}
 }
 
-func calc(n int) int {
+func calc(n int, pts int) int {
 	if n%3 == 0 {
-		return initialize(n)
+		return initialize(n, pts)
 	}
 	return n
 }
 
-func initialize(n int) int {
-	doOnce.Do(func() {
+func initialize(n int, pts int) int {
+	initFct := func(p int) {
 		fmt.Printf("Initialize (%v)", n)
-		for i := 0; i < 10; i++ {
+		for i := 0; i < p; i++ {
 			time.Sleep(1 * time.Second)
 			fmt.Print(".")
 		}
 		fmt.Println()
+	}
+
+	doOnce.Do(func() {
+		initFct(pts)
 	})
 	fmt.Printf("done (%v)\n", n)
 

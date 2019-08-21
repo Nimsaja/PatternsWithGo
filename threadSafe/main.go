48b331a56a6ad7cae44956426172e7d1f49c499e
Nimsaja <jasmin.roeper@nexgo.de>
2019-08-21 15:52:41 +0200

    atomic

--- threadSafe/main.go
+++ threadSafe/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,6 +13,8 @@ type Number struct {
 }
 
 var nb = new(Number)
+
+var nbAtomic = int32(0)
 
 func main() {
 	go safeIncrease()
@@ -28,6 +31,23 @@ func main() {
 	time.Sleep(time.Second * 1)
 
 	printOut("FIN")
+
+	fmt.Println("***********************************")
+
+	go increaseAtomic()
+	printOutAtomic("1")
+	go increaseAtomic()
+	printOutAtomic("2")
+	go increaseAtomic()
+	printOutAtomic("3")
+	go increaseAtomic()
+	printOutAtomic("4")
+	go increaseAtomic()
+	printOutAtomic("5")
+
+	time.Sleep(time.Second * 1)
+
+	printOutAtomic("FIN")
 }
 
 func safeIncrease() {
@@ -47,4 +67,19 @@ func (number *Number) increase() {
 	number.n++
 	time.Sleep(time.Second * 1)
 	fmt.Println("increase done ", number.n)
+}
+
+func increaseAtomic() {
+	nn := atomic.LoadInt32(&nbAtomic)
+	fmt.Println("increase ", nn)
+
+	nn++
+	atomic.StoreInt32(&nbAtomic, nn)
+	time.Sleep(time.Second * 1)
+
+	fmt.Println("increase done ", nn)
+}
+
+func printOutAtomic(s string) {
+	fmt.Printf("%v -> %v\n", s, atomic.LoadInt32(&nbAtomic))
 }

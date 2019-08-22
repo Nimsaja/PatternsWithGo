3416527de980a50d929f440f0b60d75eaa323e86
Nimsaja <jasmin.roeper@nexgo.de>
2019-08-22 17:36:46 +0200

    clean up

--- threadSafe/main.go
+++ threadSafe/main.go
@@ -7,30 +7,25 @@ import (
 	"time"
 )
 
-type Number struct {
-	sync.RWMutex
-	n int
-}
-
-var nb = new(Number)
-
 var nbAtomic = int32(0)
 
 func main() {
-	go safeIncrease()
-	printOut("1")
-	go safeIncrease()
-	printOut("2")
-	go safeIncrease()
-	printOut("3")
-	go safeIncrease()
-	printOut("4")
-	go safeIncrease()
-	printOut("5")
+	var nb = new(Number)
+
+	go nb.safeIncrease()
+	nb.printOut("1")
+	go nb.safeIncrease()
+	nb.printOut("2")
+	go nb.safeIncrease()
+	nb.printOut("3")
+	go nb.safeIncrease()
+	nb.printOut("4")
+	go nb.safeIncrease()
+	nb.printOut("5")
 
 	time.Sleep(time.Second * 1)
 
-	printOut("FIN")
+	nb.printOut("FIN")
 
 	fmt.Println("***********************************")
 
@@ -50,16 +45,9 @@ func main() {
 	printOutAtomic("FIN")
 }
 
-func safeIncrease() {
-	nb.Lock()
-	defer nb.Unlock()
-	nb.increase()
-}
-
-func printOut(s string) {
-	nb.Lock()
-	defer nb.Unlock()
-	fmt.Printf("%v -> %v\n", s, nb.n)
+type Number struct {
+	sync.RWMutex
+	n int
 }
 
 func (number *Number) increase() {
@@ -67,6 +55,18 @@ func (number *Number) increase() {
 	number.n++
 	time.Sleep(time.Second * 1)
 	fmt.Println("increase done ", number.n)
+}
+
+func (number *Number) safeIncrease() {
+	number.Lock()
+	defer number.Unlock()
+	number.increase()
+}
+
+func (number *Number) printOut(s string) {
+	number.RLock()
+	defer number.RUnlock()
+	fmt.Printf("%v -> %v\n", s, number.n)
 }
 
 func increaseAtomic() {

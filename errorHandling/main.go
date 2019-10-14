8e9fd7deb529a32d1738f2b8f3edc5728f314343
Nimsaja <jasmin.roeper@nexgo.de>
2019-10-14 14:18:24 +0200

    using error types

--- errorHandling/main.go
+++ errorHandling/main.go
@@ -2,25 +2,65 @@ package main
 
 import "fmt"
 
+type isZeroError struct {
+}
+
+func (e *isZeroError) Error() string {
+	return fmt.Sprintf("Error: is zero")
+}
+
+type isTooBigError struct {
+	arg int
+}
+
+func (e *isTooBigError) Error() string {
+	return fmt.Sprintf("Error: is too big: %v", e.arg)
+}
+
+type isTooLowError struct {
+	arg int
+}
+
+func (e *isTooLowError) Error() string {
+	return fmt.Sprintf("Error: is too low: %v", e.arg)
+}
+
 func main() {
-	handleError(doSomething(0))
-	handleError(doSomething(-1))
-	handleError(doSomething(1))
+	for i := 0; i < 10; i++ {
+		fmt.Print("Please put in a number: ")
+		var guess int
+		_, err := fmt.Scanf("%d", &guess)
+
+		if err != nil {
+			handleError(err)
+		} else {
+			handleError(doSomething(guess))
+		}
+	}
 }
 
 func doSomething(i int) error {
 	switch {
 	case i == 0:
-		return fmt.Errorf("****0*****")
+		return &isZeroError{}
 	case i > 0:
-		return fmt.Errorf("****i > 0 **** %v", i)
+		return &isTooBigError{i}
 	case i < 0:
-		return fmt.Errorf("****i < 0 **** %v", i)
+		return &isTooLowError{i}
+	default:
+		return nil
 	}
-
-	return fmt.Errorf("")
 }
 
 func handleError(err error) {
-	fmt.Printf("Error: %v\n", err)
+	switch err.(type) {
+	case *isZeroError:
+		fmt.Printf("%v. Please choose a number !=0 !\n", err)
+	case *isTooBigError:
+		fmt.Printf("%v. Please choose a number <= 0!\n", err)
+	case *isTooLowError:
+		fmt.Printf("%v. Please choose a number >= 0!\n", err)
+	default:
+		fmt.Printf("Error: %v\n", err)
+	}
 }
